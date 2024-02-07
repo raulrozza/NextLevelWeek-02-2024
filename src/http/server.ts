8a76932b33df43b0c2bb72b61999d36355330f26
Raul Mendes Rosá <raul.hmetal@gmail.com>
2024-02-07 12:23:29 -0300

    refactor: movindo routes and prisma to other structures

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,25 +1,6 @@
-import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
-import z from 'zod';
 
 const app = fastify();
-const prisma = new PrismaClient();
-
-app.post('/polls', async (request, reply) => {
-    const createBodySchema = z.object({
-        title: z.string(),
-    });
-
-    const { title } = createBodySchema.parse(request.body);
-
-    const poll = await prisma.poll.create({
-        data: {
-            title,
-        },
-    });
-
-    return await reply.status(201).send({ pollId: poll.id });
-});
 
 app.listen({ port: 3333 })
     .then(() => {
