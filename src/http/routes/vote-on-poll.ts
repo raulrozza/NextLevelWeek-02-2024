012fc4c4f8c30ca149b84b8cd7a5811a1352969b
Raul Mendes Rosá <raul.hmetal@gmail.com>
2024-02-07 16:26:22 -0300

    feat: added vote on poll functionality

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,6 +1,11 @@
-import { type FastifyInstance } from 'fastify';
+import {
+  type FastifyRequest,
+  type FastifyInstance,
+  type FastifyReply,
+} from 'fastify';
 import { prisma } from '../../lib/prisma';
 import z from 'zod';
+import { randomUUID } from 'crypto';
 
 const paramsSchema = z.object({
   pollId: z.string().uuid(),
@@ -9,11 +14,58 @@ const bodySchema = z.object({
   pollOptionId: z.string().uuid(),
 });
 
+const COOKIE_VALIDITY = 60 * 60 * 24 * 30; // 30 days
+
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
     const { pollId } = paramsSchema.parse(request.params);
     const { pollOptionId } = bodySchema.parse(request.body);
 
+    const sessionId = getSessionId(request, reply);
+
+    const previousVote = await prisma.vote.findUnique({
+      where: {
+        sessionId_pollId: {
+          pollId,
+          sessionId,
+        },
+      },
+    });
+
+    if (previousVote?.pollOptionId === pollOptionId)
+      return reply.status(400).send({
+        message: 'You have already voted on this poll',
+      });
+
+    if (previousVote) {
+      await prisma.vote.delete({ where: { id: previousVote.id } });
+    }
+
+    await prisma.vote.create({
+      data: {
+        pollId,
+        pollOptionId,
+        sessionId,
+      },
+    });
+
     return reply.status(201).send();
   });
+
+  function getSessionId(request: FastifyRequest, reply: FastifyReply) {
+    const storedCookie = request.cookies.sessionId;
+
+    if (storedCookie) return storedCookie;
+
+    const sessionId = randomUUID();
+
+    reply.setCookie('sessionId', sessionId, {
+      path: '/',
+      maxAge: COOKIE_VALIDITY,
+      signed: true,
+      httpOnly: true,
+    });
+
+    return sessionId;
+  }
 }
--- src/http/server.ts
+++ src/http/server.ts
@@ -2,8 +2,14 @@ import fastify from 'fastify';
 import { getPoll } from './routes/get-poll';
 import { createPoll } from './routes/create-poll';
 import { voteOnPoll } from './routes/vote-on-poll';
+import fastifyCookie from '@fastify/cookie';
 
 const app = fastify();
+
+app.register(fastifyCookie, {
+    secret: 'polls-app-nlw',
+    hook: 'onRequest',
+});
 
 app.register(createPoll);
 app.register(getPoll);
