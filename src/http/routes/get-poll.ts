1ed531e3a7ef0eeddde40bf73cdeac7a89996f35
Raul Mendes Rosá <raul.hmetal@gmail.com>
2024-02-08 12:24:40 -0300

    feat: returning results for each poll option on get

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { type FastifyInstance } from 'fastify';
 import { prisma } from '../../lib/prisma';
 import z from 'zod';
+import { redis } from '../../lib/redis';
 
 const schema = z.object({
   pollId: z.string().uuid(),
@@ -24,6 +25,32 @@ export async function getPoll(app: FastifyInstance) {
       },
     });
 
-    return reply.send({ poll });
+    if (!poll) reply.status(400).send({ message: 'Poll not found' });
+
+    const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES');
+    const votes = result.reduce<Record<string, number>>((obj, line, index) => {
+      const isEven = index % 2;
+
+      if (isEven) {
+        const score = result[index + 1];
+
+        return {
+          ...obj,
+          [line]: Number(score),
+        };
+      }
+
+      return obj;
+    }, {});
+
+    return reply.send({
+      poll: {
+        ...poll,
+        options: poll?.options.map(option => ({
+          ...option,
+          score: votes[option.id] || 0,
+        })),
+      },
+    });
   });
 }
